f39549328719a136b53cf07716c53c67fff18ea2
Angular Builds <circleci@angular.io>
2019-04-04 17:12:50 +0000

    9ac6bfd feat(@ngtools/webpack): redirect ngcc errors and warnings to webpack

--- src/build/index.d.ts
+++ src/build/index.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { BuilderContext, BuilderOutput } from '@angular-devkit/architect/src/index2';
+import { BuilderContext, BuilderOutput } from '@angular-devkit/architect';
 import { Observable } from 'rxjs';
 import { Schema as NgPackagrBuilderOptions } from './schema';
 export declare function execute(options: NgPackagrBuilderOptions, context: BuilderContext): Observable<BuilderOutput>;
